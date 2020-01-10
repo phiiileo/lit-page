a4360acef5279c6532aa3e415b966aa74bdba30b
phiiileo <emmanuelboluwajib1@gmail.com>
2020-01-10 22:48:51 +0100

    added link to the footer

--- Fragments/footer.php
+++ Fragments/footer.php
@@ -2,16 +2,16 @@
 <footer class="">
     <section class="footer_content">
         <section class="footer_content--one">
-            <a href="#" class="">About Us</a>
-            <a href="index.html" class="">
+            <a href="about-us.php" class="">About Us</a>
+            <a href="index.php" class="">
                 <img src="images/footer_logo.png" alt="" width="100px" height="100px"></a>
-            <a href="#" class="">Contact Us</a>
+            <a href="contact-us.php" class="">Contact Us</a>
         </section>
         <section class="footer_content--two">
             <a href="#"><i class="fab fa-whatsapp"></i></a>
-            <a href=""><i class="fab fa-instagram"></i></a>
-            <a href=""><i class="fab fa-twitter"></i></a>
-            <a href=""><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
         </section>
     </section>
 </footer>
