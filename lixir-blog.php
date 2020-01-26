a8acf2f3cecf28d26d2906d524dbf0472f1b681f
phiiileo <emmanuelboluwajib1@gmail.com>
2020-01-26 14:57:43 +0100

    initiated blog page

---
+++ lixir-blog.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog | Lixir</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/lixir-blog.css">
+</head>
+
+<body>
+    <?php require_once "./Fragments/header.php" ?>
+    <main>
+        <section class="blog-header">
+            <h1>Lixir News Update</h1>
+        </section>
+        <section class="blog-body">
+            <section class="blog-body_content">
+                <article class="blog-post">
+                    <div class="blog-post_image">
+                        <img src="./images/banner2.jpg" alt="">
+                    </div>
+                    <div class="blog-post_text">
+                        <h3>Post heading</h3>
+                        <span class="post-date">January 30, 2020.</span>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nihil odit officia
+                            suscipit unde et veniam dolor dignissimos adipisci mollitia molestias consectetur minima
+                            ducimus nesciunt, numquam perspiciatis sequi provident quis fugiat culpa. Error tenetur,
+                            saepe culpa optio, unde repellat voluptate, praesentium temporibus corporis nemo dicta aut
+                            a. Eius suscipit explicabo aut accusantium dolore quidem blanditiis porro voluptates labore
+                            facere quis iusto ad nobis, maiores error ipsam reiciendis distinctio corporis nemo itaque
+                            repellat voluptatem natus! Vero labore iusto quo earum sit voluptates perspiciatis animi
+                            voluptatum itaque doloribus veniam pariatur, harum consequuntur quod nam minus qui optio eum
+                            vel consequatur voluptatibus quos alias perferendis est! Adipisci eveniet exercitationem
+                            impedit ut natus? Error ea fugiat sint, cumque sed consequuntur. Minima ut delectus facere
+                            voluptate consequatur molestiae, nisi a iure! Sequi dolor rem quo enim consequatur, adipisci
+                            aperiam. Temporibus nisi ab rem debitis! Perferendis libero laborum dolores cupiditate
+                            debitis sit sapiente architecto sequi nemo in ea suscipit, atque rerum eligendi optio iure
+                            voluptates, facilis molestias veritatis ducimus amet. Quas modi illo amet natus officiis
+                            nulla, consequuntur vero ex quod harum hic ducimus, mollitia accusamus dolores quia! Culpa
+                            dicta placeat ex excepturi minus. Distinctio accusamus perspiciatis eaque ratione assumenda
+                            fugiat corporis aperiam alias aut nihil rem consectetur dolorem, laboriosam repellat
+                            quisquam ex expedita id pariatur numquam. Laborum ad molestias id facilis aliquid dolorum,
+                            ut, nulla atque aperiam nobis sed perspiciatis necessitatibus sequi nostrum velit explicabo
+                            vel cum in iste commodi blanditiis tenetur nemo recusandae! Quas enim quasi facilis
+                            veritatis eveniet labore ea assumenda deleniti vitae ab harum ipsum nulla eaque aperiam eius
+                            ad molestias in, temporibus dolore earum ex sed quam saepe possimus! Officia minus mollitia
+                            eos temporibus soluta at, minima aliquid possimus, nobis quae quam quibusdam nostrum nemo
+                            adipisci exercitationem, beatae molestias ea impedit porro aliquam repellendus autem
+                            excepturi? Sed vel quia asperiores quaerat animi? Quam, dicta repudiandae voluptate
+                            repellendus nam doloremque ipsa quis officiis iusto molestias eius tempora error minus
+                            voluptas repellat deserunt. In, quibusdam. Esse non veniam inventore ipsa unde eaque fuga
+                            accusantium officiis sint. Libero quos impedit delectus totam dolorum inventore provident
+                            ratione reiciendis in sunt! Inventore, in voluptatum unde rem totam saepe nam illo non
+                            dolorum architecto incidunt provident alias eligendi facilis sit odio repellat. Sint
+                            doloribus sunt perferendis, architecto totam voluptatibus minus cumque odit unde vitae, non
+                            rerum accusantium ea distinctio porro officiis? Porro tenetur error expedita illo magnam!
+                            Voluptate aspernatur natus quis repellendus laboriosam inventore fugiat at numquam?
+
+                        </p>
+                        <button>>> Read more</button>
+                    </div>
+                </article>
+            </section>
+            <aside class="blog-body_aside">
+               <section class="search-blog">
+                   <input type="text" placeholder="Search here...">
+                   <button><i class="fa fa-serach"></i></button>
+               </section>
+            </aside>
+        </section>
+    </main>
+    <?php require_once "./Fragments/footer.php" ?>
+</body>
+
+</html>
